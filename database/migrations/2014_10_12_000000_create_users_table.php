dab575999575f7670985272e3e575c2800657820
Elvin-Hirwa-Ndoli <elvin.h@academicbridge.xyz>
2022-10-29 11:12:37 +0200

    migrate

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,8 +15,15 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('firstname');
+            $table->string('lastname');
             $table->string('email')->unique();
+            $table->string('phone_number');
+            $table->string('district_location');
+            $table->enum('role' ,['admin','facilitator','player']);
+            $table->date('dob');
+            $table->enum('gender',['male','female']);
+            $table->string('profile_picture');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
