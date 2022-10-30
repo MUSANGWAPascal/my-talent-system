a19ccc492ec80f9953bbae9744b24d90a5784c20
MUSANGWAPascal <musangwapascal1509@gmail.com>
2022-10-30 13:20:42 +0200

    Dealing with frontend part of the web for the first time

---
+++ resources/views/index.blade.php
@@ -0,0 +1,154 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+  </head>
+  <body>
+    <header class="container">
+        <nav class="navbar navbar-expand-lg bg-light">
+            <div class="container">
+              <a class="navbar-brand" href="#">MY TALENT SYSTEM</a>
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                  <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">Register</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">Login</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </nav>
+    </header>
+
+    <main class="container mt-3">
+        <section class="row">
+            <div class="col">
+                <video class="w-100" autoplay loop muted>
+                    <source src="assets/videos/umuturanyi.mp4" type="video/mp4" />
+                </video>
+                <div class="row">
+                    <a href="single.html"><h4>UMUTURANYI SERIES</h4></a>
+                    <p>
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptate adipisci cumque soluta quibusdam 
+                    </p>
+                </div>
+            </div>
+            <div class="col">
+                <video class="w-100" autoplay loop muted>
+                    <source src="assets/videos/umuturanyi.mp4" type="video/mp4" />
+                </video>
+                <div class="row">
+                    <a href="single.html"><h4>UMUTURANYI SERIES</h4></a>
+                    <p>
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptate adipisci cumque soluta quibusdam 
+                    </p>
+                </div>
+            </div>
+            <div class="col">
+                <video class="w-100" autoplay loop muted>
+                    <source src="assets/videos/umuturanyi.mp4" type="video/mp4" />
+                </video>
+                <div class="row">
+                    <a href="single.html"><h4>UMUTURANYI SERIES</h4></a>
+                    <p>
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptate adipisci cumque soluta quibusdam 
+                    </p>
+                </div>
+            </div>
+            <div class="col">
+                <video class="w-100" autoplay loop muted>
+                    <source src="assets/videos/umuturanyi.mp4" type="video/mp4" />
+                </video>
+                <div class="row">
+                    <a href="single.html"><h4>UMUTURANYI SERIES</h4></a>
+                    <p>
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptate adipisci cumque soluta quibusdam 
+                    </p>
+                </div>
+            </div>
+        </section>
+        <section class="row">
+            <div class="col">
+                <video class="w-100" autoplay loop muted>
+                    <source src="assets/videos/umuturanyi.mp4" type="video/mp4" />
+                </video>
+                <div class="row">
+                    <a href="single.html"><h4>UMUTURANYI SERIES</h4></a>
+                    <p>
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptate adipisci cumque soluta quibusdam 
+                    </p>
+                </div>
+            </div>
+            <div class="col">
+                <video class="w-100" autoplay loop muted>
+                    <source src="assets/videos/umuturanyi.mp4" type="video/mp4" />
+                </video>
+                <div class="row">
+                    <a href="single.html"><h4>UMUTURANYI SERIES</h4></a>
+                    <p>
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptate adipisci cumque soluta quibusdam 
+                    </p>
+                </div>
+            </div>
+            <div class="col">
+                <video class="w-100" autoplay loop muted>
+                    <source src="assets/videos/umuturanyi.mp4" type="video/mp4" />
+                </video>
+                <div class="row">
+                    <a href="single.html"><h4>UMUTURANYI SERIES</h4></a>
+                    <p>
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptate adipisci cumque soluta quibusdam 
+                    </p>
+                </div>
+            </div>
+            <div class="col">
+                <video class="w-100" autoplay loop muted>
+                    <source src="assets/videos/umuturanyi.mp4" type="video/mp4" />
+                </video>
+                <div class="row">
+                    <a href="single.html"><h4>UMUTURANYI SERIES</h4></a>
+                    <p>
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptate adipisci cumque soluta quibusdam 
+                    </p>
+                </div>
+            </div>
+        </section>
+    </main>
+    <footer class="bg-dark w-100">
+        <div class="row">
+            <div class="col">
+                <p class="text-light">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam deleniti eaque nihil modi perspiciatis enim eius, recusandae, repudiandae excepturi quas ab, illum odio aliquam. Impedit rem deserunt tempora odit aliquam?
+                </p>
+            </div>
+            <div class="col">
+                <p class="text-light">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam deleniti eaque nihil modi perspiciatis enim eius, recusandae, repudiandae excepturi quas ab, illum odio aliquam. Impedit rem deserunt tempora odit aliquam?
+                </p>
+            </div>
+            <div class="col">
+                <p class="text-light">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam deleniti eaque nihil modi perspiciatis enim eius, recusandae, repudiandae excepturi quas ab, illum odio aliquam. Impedit rem deserunt tempora odit aliquam?
+                </p>
+            </div>
+        </div>
+        <div class="row">
+            <p class="text-danger text-center display-5">
+                Copyright @ 2022. My Talent System. All Right Reserved
+            </p>
+        </div>
+    </footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+  </body>
+</html>
